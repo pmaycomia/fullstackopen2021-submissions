efd5823319d018f9ba9a2b2423e38886b8f48fe5
pmaycomia <pmaycomia@gmail.com>
2021-04-06 22:30:01 +0200

    Add step 1 of phonebook

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,24 +1,55 @@
 import React, { useState } from 'react'
 
+
+
+const ShowPerson = (props) => {
+  return (
+    <div>
+    {props.name}
+    </div>
+  )
+  
+}
+
 const App = () => {
   const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas'}
   ]) 
   const [ newName, setNewName ] = useState('')
+
+  const addName = (event) => {
+    event.preventDefault()
+
+    const phonebookObject = {
+      name: newName   
+      
+    }
+    setPersons(persons.concat(phonebookObject))
+    setNewName('')
+    console.log(event.target);
+  }
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addName}>
+     
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      {persons.map(list => 
+          <ShowPerson key={list.id} name={list.name} /> )}
+
+      <div> debug: {newName} includes() or find ()</div>
     </div>
   )
 }
